b356953d58e8adb77e6f8961662e8f093fca42d4
tompng <tomoya@tomoya-Air.local>
2012-11-30 23:10:14 +0900

    remove dup portnumber

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 export NODE_PORT=8080
 export NODE_UPORT=8000
-export RAILS_PORT=8090
+export RAILS_PORT=80
 export CONSUMER_KEY='pJ2F7coxlc5jlKcmSPrLqQ';
 export CONSUMER_SECRET='n0qURhYbR8ugJfET12fhb1ko4vJD18e8Kmuxlk3M884'
 
@@ -10,6 +10,5 @@ cd NodeXServer
 rm nohup.out
 nohup node app.js&
 cd ../RailsXServer
-rm nohup.out
-nohup rails s -p $RAILS_PORT&
+rails s -d -p $RAILS_PORT
 
