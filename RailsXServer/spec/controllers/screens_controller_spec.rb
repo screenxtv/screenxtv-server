e5c4ca416f08249cec5ff64dd71ea5dc858a3678
tompng <tomoyapenguin@gmail.com>
2013-05-07 04:50:20 +0900

    add test

---
+++ RailsXServer/spec/controllers/screens_controller_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+
+describe ScreensController do
+  before do
+    @user=User.new_account name:'tompng',email:'e@mail',password:'pswd'
+    @user.save
+  end
+  context 'views' do
+    it 'should respond to embed' do
+      get :show_embed,url:'foo'
+      assigns[:title].should eq 'foo'
+      assigns[:url].should eq 'foo'
+      response.should be_success
+      response.should render_template 'embed'
+    end
+    context 'public' do
+
+    end
+    context 'private' do
+      context 'get' do
+        before do
+          get :show_private,url:'foo'
+        end
+        it 'assings' do
+          assigns.should include(title:'foo',url:'private/foo',private:true,chats:[])
+        end
+        context 'response' do
+          subject{response}
+          it{should be_success}
+          it{should render_template 'show'}
+        end
+      end
+      context 'chat' do
+        before{get :show_private,url:'foo',chat:''}
+        subject{response}
+        it{should be_success}
+        it{should render_template 'chat'}
+      end
+    end
+
+    context 'status api' do
+      it 'success get url' do
+        get :status, url:'hogehoge'
+        response.should be_success
+      end
+      it 'success get url title' do
+        get :status, url:'hogehoge', key:'title'
+        response.should be_success
+        response.body.should eq 'null'
+      end
+      it 'success get existing url' do
+        data={title:'hoge',color:'red',viewer:0,casting:true}
+        params={title:data[:title],color:data[:color],current_viewer:data[:viewer],state:Screen::STATE_CASTING}
+        @user.screens.first.update_attributes(params)
+        get :status, url:'tompng'
+        response.should be_success
+        response.body.should eq data.to_json
+        get :status, url:'tompng', key:'title'
+        response.should be_success
+        response.body.should eq data[:title]
+      end
+    end
+  end
+
+end
+
