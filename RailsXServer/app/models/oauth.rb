f46856e8bc13974ba30e19f19404bc7fa61419c9
tompng <tomoyapenguin@gmail.com>
2013-05-18 15:19:20 +0900

    fix Oauth.url_for when nil

--- RailsXServer/app/models/oauth.rb
+++ RailsXServer/app/models/oauth.rb
@@ -14,6 +14,7 @@ class Oauth < ActiveRecord::Base
   end
 
   def self.url_for oauth
+    return unless oauth
     case oauth[:provider]
     when 'twitter'
       "http://twitter.com/#{oauth[:name]}"
